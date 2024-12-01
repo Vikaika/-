a1c46cb88627a764731cd8997b3b6ec9563eed88
Gunko Vika <vikahunko@icloud.com>
2024-12-02 01:17:36 +0300

    Update FileProcessing.java

--- FileProcessing.java
+++ FileProcessing.java
@@ -2,7 +2,10 @@ package com.example.proj;
 
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+public interface FileProcessing {
+    ArrayList<String> getStringArray(File file) throws IOException, ParserConfigurationException, SAXException;
+}
