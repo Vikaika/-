40dea84c52e60cfccbb3240fe4567f84717ab7b7
Gunko Vika <vikahunko@icloud.com>
2024-12-02 00:45:57 +0300

    Add files via upload

---
+++ ReadWriteFiles.java
@@ -0,0 +1,9 @@
+package com.example.semproj;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface ReadWriteFiles {
+    String read(File file) throws IOException;
+    void write(File file, String str) throws IOException;
+}
