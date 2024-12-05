3fe0cc2a60cbe79d4b22d2f63f85ac5c8d2f1398
Gunko Vika <vikahunko@icloud.com>
2024-12-05 23:29:54 +0300

    Update TextFilesReadWrite.java

--- TextFilesReadWrite.java
+++ TextFilesReadWrite.java
@@ -1,4 +1,4 @@
-//чтение данных из текстового файла
+//чтение содержимого текстовых файлов
 package com.example.proj;
 
 import java.io.File;
@@ -6,15 +6,17 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//чтение строк из файла и возвращение их в виде списка 
 public class TextFilesReadWrite implements FileProcessing {
     @Override
+    //чтение строк из файла и возвращение их в виде списка
     public ArrayList<String> getStringArray(File file) throws FileNotFoundException {
         ArrayList<String> text = new ArrayList<>();
         Scanner scanner = new Scanner(file);
         while (scanner.hasNext()) {
             text.add(scanner.nextLine());
         }
-        
+
         return text;
     }
 }
