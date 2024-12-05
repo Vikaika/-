91a1b0f7f55805a4884c5bbab6f8a6b3641c0553
Gunko Vika <vikahunko@icloud.com>
2024-12-06 00:33:58 +0300

    Update XMLFilesReadWrite.java

--- XMLFilesReadWrite.java
+++ XMLFilesReadWrite.java
@@ -11,4 +11,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class XMLFilesReadWrite implements FileProcessing {}
+public class XMLFilesReadWrite implements FileProcessing {
+        ArrayList<String> text = new ArrayList<>();
+        /* создание нового объекта DocumentBuilderFactory,
+         который может создавать парсеры */
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        /* parse() читает XML-документ и создаёт объект document,
+        который содержит дерево элементов XML и содержит данные, полученные в результате парсинга */
+        Document document = builderFactory.newDocumentBuilder().parse(file);
+
+}
