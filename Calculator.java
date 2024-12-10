c1b355f97e2c4c72cb2ecf8214d2fafdeaa30804
Gunko Vika <vikahunko@icloud.com>
2024-12-11 00:47:32 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
+  private final Stack<Double> values = new Stack<>(); //стек для числовых значений
+  private final Stack<String> operators = new Stack<>(); //стек для операторов
 }
