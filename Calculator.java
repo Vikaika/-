8be1d7db11f4c7c1d1e5a1c6809c736dc6da4be6
Gunko Vika <vikahunko@icloud.com>
2024-12-16 20:52:56 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,24 +6,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-  private final Stack<Double> values = new Stack<>(); //стек для числовых значений
-  private final Stack<String> operators = new Stack<>(); //стек для операторов
+    private final Stack<Double> values = new Stack<>(); //стек для числовых значений
+    private final Stack<String> operators = new Stack<>(); //стек для операторов
 
-  private static final Pattern operatorr = Pattern.compile("[/*\\-+^]"); //поиск одного из операторов
-  private static final Pattern double_operator = Pattern.compile("[/*\\-+^]{2}"); //поиск двух подряд идущих символа
-  private static final Pattern digit = Pattern.compile("\\d+(\\.\\d+)?"); //поиск целые и дробные числа
+    private static final Pattern one_operator = Pattern.compile("[/*\\-+^]"); //поиск одного из операторов
+    private static final Pattern double_operator = Pattern.compile("[/*\\-+^]{2}"); //поиск двух подряд идущих символа
+    private static final Pattern numbers = Pattern.compile("\\d+(\\.\\d+)?"); //поиск целые и дробные числа
 
-  //метод для вычисления выражения
-  public String calculate(String expression) {
-    String extra_space = sanitizeExpression(expression); //очищаем выражение от лишних пробелов и исправляем ошибки формата
-    if (isValidExpression(extra_space)) {
-      to_RPN(extra_space); //чтобы не отслеживать приоритеты операций во время вычислений
-      return " " + evaluate() + " "; //выполняем вычисления на основе выражения в RPN
+    //метод для вычисления выражения
+    public String calculate(String str) {
+        String extra_space = sanitizeExpression(str); //убираем лишние пробелы и исправляем ошибки формата
+        if (isValidExpression(extra_space)) {
+            RPN(extra_space);
+            return " " + evaluate() + " "; //выполняем вычисления на основе выражения в RPN
+        }
+        return str;
     }
-   return expression;
-  }
+
+    //чтобы не отслеживать приоритет операций
+    private void RPN(String str) {
+        StringTokenizer tokens = new StringTokenizer(str, "/*-+^()", true); //разбиваем строку на токены
+        while (tokens.hasMoreTokens()) {
+            String token = tokens.nextToken();
 
 
-
-  
+        }
+    }
 }
