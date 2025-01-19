6ac2a0a52e12af0a57d19ba7baa107464cc47259
Gunko Vika <vikahunko@icloud.com>
2025-01-19 20:07:31 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -21,14 +21,14 @@ public class Calculator {
     private static final Pattern double_operator = Pattern.compile("[/*\\-+^]{2}"); //поиск двух подряд идущих символа
     private static final Pattern numbers = Pattern.compile("\\d+(\\.\\d+)?"); //поиск целые и дробные числа
 
-    //метод для вычисления выражения
+    // метод для вычисления выражения
     public String Calculate(String str) {
-        String extra_space = Clearing(str); //убираем лишние пробелы и исправляем ошибки формата
-        if (ValidExpression(extra_space)) {
-            RPN(extra_space);
-            return " " + Result() + " "; //выполняем вычисления на основе выражения в RPN
+        String extra_space = Clearing(str); // убираем лишние пробелы и исправляем ошибки формата
+        if (!ValidExpression(extra_space)) {
+            throw new IllegalArgumentException("Некорректное выражение");
         }
-        return str;
+        RPN(extra_space);
+        return Result(); // возвращаем результат без лишних пробелов
     }
 
     //чтобы не отслеживать приоритет операций
@@ -48,18 +48,21 @@ public class Calculator {
 
     //метод для управления логикой вычисления выражений, учитывая приоритет и порядок выполнения операций
     private void ProcessOperator(String operator) {
-        //приоритет текущего оператора должен быть меньше или равен приоритету верхнего оператора в стеке
         while (!operators.isEmpty() && OperationPriority(operator) <= OperationPriority(operators.peek())) {
-            //игнорируем ")"
             if (operator.equals(")")) {
                 operators.pop();
                 return;
             }
+
+            if (values.size() < 2) {
+                throw new IllegalArgumentException("Недостаточно значений в выражении");
+            }
+
             double second_val = values.pop();
             double first_val = values.pop();
             values.push(PerformOperation(first_val, second_val, operators.pop()));
         }
-        //если оператор это не ")", добавляем его в стек
+
         if (!operator.equals(")")) {
             operators.push(operator);
         }
@@ -67,8 +70,10 @@ public class Calculator {
 
     //метод по очистке и подготовке выражения для вычисления
     private String Clearing(String expression) {
-        expression = expression.replaceAll(" ", ""); //удаляем все пробелы
-        if (expression.charAt(0) == '-') expression = "0" + expression; //если первый символ "-"
+        expression = expression.replaceAll(" ", ""); // удаляем все пробелы
+        if (expression.charAt(0) == '-') expression = "0" + expression; // если первый символ "-"
+
+        // Обработка отрицательных чисел
         return expression.replaceAll("\\(-", "(0-")
                 .replaceAll("(\\d)\\(", "$1*(")
                 .replaceAll("\\)\\(", ")*(")
